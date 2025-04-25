04e72d4f5f579291b474370ad80f7cb9bf704f52
GianES26 <a20224926@pucp.edu.pe>
2025-04-25 16:36:40 -0500

    Creacion repositorio

---
+++ src/main/java/com/example/intentolab3/IntentoLab3Application.java
@@ -0,0 +1,13 @@
+package com.example.intentolab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntentoLab3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntentoLab3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/intentolab3/IntentoLab3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intentolab3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntentoLab3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
